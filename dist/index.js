42cffe4ed7af1ea2f7f01832c7d2f54ac82718ce
ArshPartior <101242136+ArshPartior@users.noreply.github.com>
2022-03-29 00:15:42 +0530

    Added custom prmessageblock

--- dist/index.js
+++ dist/index.js
@@ -14543,6 +14543,133 @@ async function getPrMessageBlock(octokit, run, definition) {
 
     return message
 }
+
+async function getMessageBlock(octokit, run, definition) {
+
+    var message = "";
+    var oldFile = "";
+    var newFile = "";
+    var diffMessage = "";
+
+    message += "# " + definition["title"] + "\n";
+
+    for (const branch of definition["compare_branches"]) {
+        message += `## Previous ${branch} branch:\n\n`;
+
+        const data = await readArchivedFile(octokit, run, branch,
+                                    definition.artifact_name,
+                                    definition.message_file,
+                                    definition.modifier)
+        
+        oldFile = data;
+        );
+    }
+
+    message += "\n## Delta File:\n\n";
+
+    const data = fs.readFileSync(definition["message_file"], 'utf8')
+    
+    newFile = data;
+    
+    diffMessage = deltaFile(oldFile,newFile);
+    message += utils.formatMarkdownBlock(
+             diffMessage,
+             definition.collapsible
+    );
+    message += "\n## Full Scan Report:\n\n";
+    message += utils.formatMarkdownBlock(
+                utils.applyMessageModifier(newFile, definition["modifier"]),
+                definition.collapsible
+    );
+    return message
+}
+
+function deltaFile(oldFile,newFile)
+{
+    var searchKey = "## ";
+    var newTitle = newFile.split("##")[0];
+    var output = "";
+
+    var oldFileArray = splitElement(oldFile,searchKey);
+    console.log("After split, Old File array is - ",oldFileArray);
+    var newFileArray = splitElement(newFile,searchKey);
+    console.log("After split, New File array is - ",newFileArray);
+    loop1: for (i=0; i<newFileArray.length; i++)
+    {
+        var result = "";
+        var reportFound = false;
+        var newReportName = newFileArray[i].split("- **UUID")[0];  
+        console.log("New report name",newReportName);
+        loop2: for (j=0; j<oldFileArray.length; j++)
+        {
+            var oldReportName = oldFileArray[j].split("- **UUID")[0]; 
+            console.log("Old report name",oldReportName);
+            if(newReportName == oldReportName)
+            {
+                reportFound = true;
+                var newFileSubArray = (splitElement(newFileArray[i],"- **UUID"));
+                console.log("newFileSubArray:::",newFileSubArray);
+                var oldFileSubArray = (splitElement(oldFileArray[j],"- **UUID"));
+                console.log("Report found")
+                     loop3: for(var k=0;k<newFileSubArray.length;k++)
+                     {
+                         var matchFound = false;
+                         var newIssue = newFileSubArray[k].split("- **Issue:**")[1].trim();
+                         console.log("newIssue:::",newIssue);
+                         loop4: for(var l=0;l<oldFileSubArray.length;l++)
+                         {
+                            var oldIssue = oldFileSubArray[l].split("- **Issue:**")[1].trim();
+                            console.log("oldIssue:::",oldIssue);
+                            if(newIssue == oldIssue)
+                            {
+                                matchFound = true;
+                                console.log("Issue matched");
+                                //oldFileSubArray.splice(l, 1);              
+                                break loop4;
+                             }            
+                         }
+                         if(!matchFound)
+                         {          
+                            console.log("Issue did not match");
+                            console.log("Before appending, result is::",result)
+                            result+=newFileSubArray[k];
+                            console.log("After appending, result is::",result)
+                         }
+                     } 
+                break loop2;
+            }
+        }
+        if(!reportFound){
+            output += newFileArray[i];
+        }
+      output = (result && result != "") ? output + newReportName+result : output;
+      console.log("After appending , report is::",output)
+    }
+var finalReport = newTitle+output;
+console.log("Final output:::",finalReport);
+return (finalReport)   
+}
+
+function splitElement(element,searchKeyword)
+{
+var allIndices = [];
+var indexOccurence = element.indexOf(searchKeyword, 0);
+while(indexOccurence >= 0) {
+    allIndices.push(indexOccurence);
+    indexOccurence = element.indexOf(searchKeyword, indexOccurence + 1);
+}
+
+console.log("All indices",allIndices);
+var splittedElement = [];
+for (var i=0 ; i<allIndices.length; i++){
+		if(i!=allIndices.length-1)
+		splittedElement.push(element.substring(allIndices[i],allIndices[i+1]));
+    else
+    splittedElement.push(element.substring(allIndices[i]));    
+}
+return (splittedElement)
+}
+
   
 function processDefinition(definition) {
 
@@ -14581,7 +14708,7 @@ async function getPrMessage(octokit, definitions) {
 
     var prMessage = ""
     for (const definition of definitions) { 
-        prMessage += await getPrMessageBlock(
+        prMessage += await getMessageBlock(
             octokit,
             run,
             definition)
@@ -14605,6 +14732,7 @@ async function postPrMessage(octokit, prNumber, prMessage) {
 module.exports = {
     readArchivedFile,
     getPrMessageBlock,
+    getMessageBlock,
     getPrMessage,
     processDefinition,
     postPrMessage
