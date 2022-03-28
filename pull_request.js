2716261da95f0b52580eb447047dc385c0f05d7a
ArshPartior <101242136+ArshPartior@users.noreply.github.com>
2022-03-29 00:39:57 +0530

    Update pull_request.js

--- pull_request.js
+++ pull_request.js
@@ -132,11 +132,13 @@ async function getMessageBlock(octokit, run, definition) {
     
     newFile = dataNew;
     
+    if (oldFile.length>0 && newFile.length>0) {
     diffMessage = deltaFile(oldFile,newFile);
     message += utils.formatMarkdownBlock(
              diffMessage,
              definition.collapsible
     );
+    }
     message += "\n## Full Scan Report:\n\n";
     message += utils.formatMarkdownBlock(
                 utils.applyMessageModifier(newFile, definition["modifier"]),
